e7593908bddd3e274114ec235c6b133b204fb0a1
TomB-S <thomas.benameur@gmail.com>
2026-01-21 13:00:49 +0100

    Revert "adding info"
    This reverts commit 81b0d4e369b5576677fcd33e951d749e771743fe.

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,6 +1,5 @@
 # Shipping Cost Calculator
  # Here is a new update by <TomB-S>
-# Here is another update by <TomB-S>
 
 ## Input package weight and shipping rate
 weight = float(input("Enter the package weight in kilograms: "))
